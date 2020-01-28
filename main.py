58958f8096ad890ca0dd530ecf2b841dc5a0b3e2
Shravan Kumar <shravankanak@gmail.com>
2020-01-28 05:00:55 +0000

    Added Exaple

---
+++ main.py
@@ -0,0 +1,32 @@
+# Default arguments values :-\/
+print("**** Example - 1 ****")
+def ask_ok(prompt, retries = 4, complaint = 'Yes or No, Please..!'):
+  while True:
+    ok = input(prompt)
+    if ok in ('Y', 'Ye', 'Yes'):
+      return True
+    if ok in ('n', 'no', 'nop', 'nope'):
+      return False
+    retries = retries - 1
+    if retries < 0:
+      raise IOError("refusenik user")
+    print(complaint)
+  
+ask_ok("ENter Yes or NO")
+
+print('\n')
+#Example - 2
+
+print("**** Example - 2 ****")
+
+def get_gender(sex = 'Unknown'):
+  if sex is 'm':
+    sex = 'MALE'
+  elif sex is 'f':
+    sex = 'FEMALE'
+  print(sex)
+
+
+get_gender('m')
+get_gender('f')
+get_gender()
