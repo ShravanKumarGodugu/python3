d281d5130380ba55d2a1e99e33e76d1309390b78
Shravan Kumar <shravankanak@gmail.com>
2020-01-28 09:01:19 +0000

    Keyword_Arguments_Example

---
+++ Keyword_Arguments.py
@@ -0,0 +1,10 @@
+# Key word arguments
+
+def sentence(name = 'Bucky', action = 'ate', item = 'Tuna'):
+  print(name, action, item)
+
+
+sentence()  # Returns Default values.
+sentence('shravan', 'ate' , 'Oranges') # Passing the Arguments.
+sentence(name = 'Wow', action = 'Yehi tho hai', item = 'Keyword_arguments') # Keyword Arguments
+
--- main.py
+++ main.py
@@ -1,32 +1,10 @@
-# Default arguments values :-\/
-print("**** Example - 1 ****")
-def ask_ok(prompt, retries = 4, complaint = 'Yes or No, Please..!'):
-  while True:
-    ok = input(prompt)
-    if ok in ('Y', 'Ye', 'Yes'):
-      return True
-    if ok in ('n', 'no', 'nop', 'nope'):
-      return False
-    retries = retries - 1
-    if retries < 0:
-      raise IOError("refusenik user")
-    print(complaint)
-  
-ask_ok("ENter Yes or NO")
+# Key word arguments
 
-print('\n')
-#Example - 2
-
-print("**** Example - 2 ****")
-
-def get_gender(sex = 'Unknown'):
-  if sex is 'm':
-    sex = 'MALE'
-  elif sex is 'f':
-    sex = 'FEMALE'
-  print(sex)
+def sentence(name = 'Bucky', action = 'ate', item = 'Tuna'):
+  print(name, action, item)
 
 
-get_gender('m')
-get_gender('f')
-get_gender()
+sentence()  # Returns Default values.
+sentence('shravan', 'ate' , 'Oranges')
+sentence(name = 'Wow', action = 'Yehi tho hai', item = 'Keyword_arguments')
+
