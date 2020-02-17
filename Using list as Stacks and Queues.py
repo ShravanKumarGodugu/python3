890541490722a293197144f30d69531eae3fde43
Shravan Kumar <shravankanak@gmail.com>
2020-02-17 10:39:45 +0000

    Example Code Added for Stacks and Deques in a list

--- Using list as Stacks and Queues.py
+++ Using list as Stacks and Queues.py
@@ -1,20 +1,41 @@
 # Using List as Stack.
+
+print("---- Using List as Stack ---- \n")
+
 stack = [6,7,8,9]
-stack.append(8)
-stack.append(10)
-print(stack)
+print("Basic LIST ===> ", stack)
+
+stack.append(100)
+stack.append(200)  # Last In 
+print("\nUsing append method created List is  ===> ", stack)
+
+# Using POP method
 stack.pop()
+print("\nUsing POP method(Retriving elements from a list (Last In First Out)  ===> ", stack)  #Last In First Out
+
 stack.pop()
-print(stack)
+print("\nFinal Output Is ===> ", stack)
 
 # Using List as Queues(collection.deque).
 
+print("")
 
+print("---- Using List as Queues(DEQUE) ----")
 from collections import deque
+
 queue = deque(['shravan','shishir','adarsh'])
-queue.append('Deepak')
+print("Basic LIST ===> ", queue)
+
+
+queue.append('Deepak')  # First In
 queue.append('Abhilash')
-print(queue)
+print("\nUsing append method created List is  ===> ", list(queue))
+
+# Using POP method
+
 queue.popleft()
-print(queue)
+print("\nFinal output Using POP LEFT METHOD ===> ", list(queue))
+
+queue.pop() 
+print("\nUsing POP method(Retriving elements from a list (First In First Out) ===> ", list(queue)) # First Out
 
